b66649871abc185897a14a13406cd05279ce329f
billychou <zsc1528@gmail.com>
2018-08-10 23:41:58 +0800

    ch1-2

--- go-startup/agent/src/ch1/1_2.go
+++ go-startup/agent/src/ch1/1_2.go
@@ -1,15 +1,25 @@
+// 1.包申明
 package main
 
+// 2. 引入包
 import (
 	"fmt"
 	"os"
 )
 
+// 主函数
 func main() {
+	var b = true
+	if b {
+		fmt.Printf("welcome")
+	}
+	// 循环语句
+	// 变量
 	var s, sep string
-	for i := 1; i< len(os.Args); i++ {
+	// 循环语句
+	for i := 1; i < len(os.Args); i++ {
 		s += sep + os.Args[i]
-		sep = " "
+		sep = "|"
 	}
 	fmt.Println(s)
 }
