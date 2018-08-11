f4b2e79bb7a14df2b477b03708f4a844dabc0ded
billychou <zsc1528@gmail.com>
2018-08-11 23:07:00 +0800

    chapt3-基本数据类型

--- go-startup/agent/src/ch1/1_2.go
+++ go-startup/agent/src/ch1/1_2.go
@@ -4,12 +4,16 @@ package main
 // 2. 引入包
 import (
 	"fmt"
+	"math"
 	"os"
 )
 
 // 主函数
 func main() {
 	var b = true
+	var u uint8 = 255
+	fmt.Println(u, u+1, u*u)
+
 	if b {
 		fmt.Printf("welcome")
 	}
@@ -22,4 +26,21 @@ func main() {
 		sep = "|"
 	}
 	fmt.Println(s)
+
+	//  整数
+	var x uint8 = 1<<1 | 1<<5
+	var y uint8 = 1<<1 | 1<<2
+	fmt.Printf("%08b\n", x)
+	fmt.Printf("%08b\n", y)
+	// 浮点数
+	// float32 float64
+	var f float32 = 16777216
+	fmt.Println(f == f+1) // true
+	// float 能精确标示的正整数范围有限
+	const e = 2.71828
+	for x := 0; x < 8; x++ {
+		fmt.Printf("x = %d e^x = %8.3f\n", x, math.Exp(float64(x)))
+	}
+	// 字符串
+
 }
