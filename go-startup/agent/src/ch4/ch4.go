4e251e6df6b9109ac04dd2d64e28c195e6394f1a
billychou <zsc1528@gmail.com>
2018-08-13 23:41:51 +0800

    Array，1. 数组的初始化 2.数组长度

---
+++ go-startup/agent/src/ch4/ch4.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a [3]int
+	fmt.Println(a[0])
+	fmt.Println(a[len(a)-1])
+	for i, v := range a {
+		fmt.Printf("%d %d\n", i, v)
+	}
+
+	for _, v := range a {
+		fmt.Printf("%d\n", v)
+	}
+
+	var q [3]int = [3]int{1, 2, 3}
+	for i, v := range q {
+		fmt.Printf("%d\t%d\n", i, v)
+	}
+	// 数组长度由初始化数组的元素个数
+	qArray := [...]int{1, 2, 3}
+	for i, v := range qArray {
+		fmt.Printf("%d\t%d\n", i, v)
+	}
+}
