9282756cde1a7253919ec0ae7392518c0f6bb8bd
billychou <zsc1528@gmail.com>
2018-09-03 23:37:58 +0800

    寄生构造函数模式

--- js-startup/chapter6/6_2_5.js
+++ js-startup/chapter6/6_2_5.js
@@ -1,10 +1,13 @@
 // 动态原型模式
+// Digest:
+// 动态原型模式把所有信息都封装在了构造函数中，而通过在构造函数中初始化原型，而通过在构造函数中初始化原型，
+// 保持了同事使用构造函数和原型的优点
 
 function Person(name, age, job) {
     this.name = name;
     this.age = age;
     this.job = job;
-    
+    // 在sayName方法不存在的情况下，会将它添加到原型中
     if (typeof this.sayName != "function") {
         Person.prototype.sayName = () => {
             console.log(this.name);
--- js-startup/chapter6/6_2_6.js
+++ js-startup/chapter6/6_2_6.js
@@ -1,11 +1,11 @@
 // 创建一个函数，仅仅封装创建对象的代码，然后再返回新建的对象
 
-const Person = (name, age, job) => {
+function Person(name, age, job) {
     let o = new Object();
     o.name = name;
     o.age = age;
     o.job = job;
-    o.sayName = ()=> {
+    o.sayName = () => {
         console.log(this.name);
     }
     return o;
